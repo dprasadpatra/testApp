e76bbdd253dd3f71a23cac233c4ba3f4bccac1a4
Durga Patra <durgaprasad.patra@asticsinc.com>
2018-07-25 15:18:20 +0530

    master

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'antd/dist/antd.css';
-import Store from "./store";
 
-ReactDOM.render(<App store={Store} />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
